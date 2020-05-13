ef4b74ee662a67ffb6acbe6dca669a2b1227cfe9
joaov56 <vitorjao56@gmail.com>
2020-05-13 13:23:22 -0300

    configurações do passport

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt")
+const passport = require("passport")
+const flash = require("express-flash")
+const session = require("express-session")
 
-app.use(express.urlencoded({ extended: false }))
+
+const initializePassport = require("./passport-config")
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
 
 const users = []
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 
@@ -22,9 +45,11 @@ app.get("/login", (req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
-
-})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
 
 
