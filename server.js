784168f2137b6778e8c22932e2050748747dc525
joaov56 <vitorjao56@gmail.com>
2020-05-12 21:33:45 -0300

    criadas as rotas

--- server.js
+++ server.js
@@ -2,12 +2,42 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: false }))
+
+
+const user = []
+
 
 
 app.get("/", (req, res) => {
+    res.render("index.ejs")
+
+})
+
+
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs")
+
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+
+
+
+app.get("/register", (req, res) => {
     res.render("register.ejs")
 
 })
+
+app.post('/register', (req, res) => {
+    console.log(req.body.name)
+})
+
+
 
 
 //USING CSS ON MY CODE
